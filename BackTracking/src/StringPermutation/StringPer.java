301ead4b914e1f526d041a5e8d8687c5f344d80a
Biswanath00 <46648233+Biswanath00@users.noreply.github.com>
2019-11-05 12:01:13 +0530

    hashset used

--- BackTracking/src/StringPermutation/StringPer.java
+++ BackTracking/src/StringPermutation/StringPer.java
@@ -1,11 +1,13 @@
 package StringPermutation;
 
+import java.util.HashSet;
 
 public  class StringPer
-  {
-     
+  { 
+    static HashSet<String>  hs = new HashSet<String>();
+    // we use hash set when character are common
   	public static void main(String[] args) {
-  		 char arr[]={'d','a','t','a'};
+  		 char arr[]={'d','a','t','a','d'};
   		 
   		Permutation(0,arr);
 
@@ -17,7 +19,11 @@ public  class StringPer
   	 	if(count==arr.length)
   	 	 {  
             String str=new String(arr);
+            if(!hs.contains(str))
+            {
   	 	 	System.out.println(arr);
+  	 	 	hs.add(str);
+            }
   	 	 	return ;
          }
         
